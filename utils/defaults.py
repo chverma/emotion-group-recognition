9675fbe127a0ded34975a8cf005a41e56bdc45e9
chverma <chverma@upv.es>
2016-05-05 16:23:06 +0200

    Change defaults

--- utils/defaults.py
+++ utils/defaults.py
@@ -4,6 +4,7 @@ dim = 12 ## bacause I use 12 distances
 
 emotions=["happy","neutral","disgust","fear","surprised","sad"]
 CLASS_N = 6 ## At the moment 6 classes
+## CSVs
 happy_csv   = '/home/chverma/UPV/TFG/data/csv/happy.csv'
 neutral_csv = '/home/chverma/UPV/TFG/data/csv/neutral.csv'
 disgust_csv = '/home/chverma/UPV/TFG/data/csv/disgust.csv'
@@ -12,14 +13,18 @@ fear_csv   = '/home/chverma/UPV/TFG/data/csv/fear.csv'
 surprised_csv = '/home/chverma/UPV/TFG/data/csv/surprised.csv'
 sad_csv = '/home/chverma/UPV/TFG/data/csv/sad.csv'
 
-neutral_imgs = '/home/chverma/UPV/TFG/data/faces/union/neutral/'
-happy_imgs = '/home/chverma/UPV/TFG/data/faces/union/happy/'
-disgust_imgs = '/home/chverma/UPV/TFG/data/faces/union/disgust/'
+##IMAGES
+img_directory = '/home/chverma/UPV/TFG/data/faces/union/'
+neutral_imgs = img_directory+'neutral/'
+happy_imgs = img_directory+'happy/'
+disgust_imgs = img_directory+'disgust/'
 
 
-fear_imgs = '/home/chverma/UPV/TFG/data/faces/union/fear/'
-surprised_imgs = '/home/chverma/UPV/TFG/data/faces/union/surprised/'
-sad_imgs = '/home/chverma/UPV/TFG/data/faces/union/sad/'
+fear_imgs = img_directory+'fear/'
+surprised_imgs = img_directory+'surprised/'
+sad_imgs = img_directory+'sad/'
 
+##SAVED MODELS
 model_svm_xml = '/home/chverma/UPV/TFG/pythonDlibLendmark/models/emotions_svm.xml'
+model_knearest_xml = '/home/chverma/UPV/TFG/pythonDlibLendmark/models/emotion_KNearest.xml'
 model_shape = '/home/chverma/UPV/TFG/pythonDlibLendmark/models/shape_predictor_68_face_landmarks.dat'
