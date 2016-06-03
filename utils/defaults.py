91774ca9f22e51ec539a1f2f44aa3bd38372a534
chverma <chverma@upv.es>
2016-06-03 15:04:17 +0200

    Adds utils.toCSV Ease dataset selection

--- utils/defaults.py
+++ utils/defaults.py
@@ -1,25 +1,29 @@
 ##########DEFAULTS
-dim = 12 ## bacause I use 12 distances
+dim = 2278 ## bacause I use 12 distances
+dataset = 'KDEF' # JAFFE, KDEF, UNION
+data_path = '/home/chverma/UPV/TFG/data/'
 
+emotions=["happy","neutral","disgust","fear","surprised","sad","angry"]
+CLASS_N = 7 ## At the moment 7 classes
+    
+use_log = True
 
-emotions=["happy","neutral","disgust","fear","surprised","sad"]
-CLASS_N = 6 ## At the moment 6 classes
 ## CSVs
-happy_csv   = '/home/chverma/UPV/TFG/data/csv/happy.csv'
-neutral_csv = '/home/chverma/UPV/TFG/data/csv/neutral.csv'
-disgust_csv = '/home/chverma/UPV/TFG/data/csv/disgust.csv'
+happy_csv   = data_path+'csv/happy.csv'
+neutral_csv = data_path+'csv/neutral.csv'
+disgust_csv = data_path+'csv/disgust.csv'
 
-fear_csv   = '/home/chverma/UPV/TFG/data/csv/fear.csv'
-surprised_csv = '/home/chverma/UPV/TFG/data/csv/surprised.csv'
-sad_csv = '/home/chverma/UPV/TFG/data/csv/sad.csv'
-
+fear_csv   = data_path+'csv/fear.csv'
+surprised_csv = data_path+'csv/surprised.csv'
+sad_csv = data_path+'csv/sad.csv'
+angry_csv   = data_path+'csv/angry.csv'
 ## IMAGES
-img_directory = '/home/chverma/UPV/TFG/data/faces/union/'
-#img_directory = '/home/chverma/UPV/TFG/data/faces/KDEF_Class/'
+img_directory = data_path+'faces/'+dataset+"/"
+
 neutral_imgs = img_directory+'neutral/'
 happy_imgs = img_directory+'happy/'
 disgust_imgs = img_directory+'disgust/'
-
+angry_imgs = img_directory+'angry/'
 fear_imgs = img_directory+'fear/'
 surprised_imgs = img_directory+'surprised/'
 sad_imgs = img_directory+'sad/'
@@ -32,8 +36,19 @@ model_boost_xml = 'models/boost6.xml'
 model_shape = 'models/shape_predictor_68_face_landmarks.dat'
 
 ## DATASET (distances and labels)
-file_dataset51 = 'dataset/dataset51.npy' 
-file_labels51 =  'dataset/labels51.npy'
+### 68 punts, calcul de TOTES les distancies
+CAFE_data = 'dataset/cafe_data.npy' 
+CAFE_labels =  'dataset/cafe_labels.npy'
+
+KDEF_data = 'dataset/kdef_data.npy' 
+KDEF_labels =  'dataset/kdef_labels.npy'
+
+UNION_data = 'dataset/union_data.npy' 
+UNION_labels = 'dataset/union_labels.npy'
+
+### VARIOS
+union_1318_nolog = 'dataset/union_1318_nolog.npy' 
+union_labels_1318_nolog =  'dataset/union_labels_1318_nolog.npy'
 
 file_dataset12 = 'dataset/dataset12.npy' 
 file_labels12 =  'dataset/labels12.npy'
@@ -47,7 +62,20 @@ file_labelsKDEF =  'dataset/fdef_labels12.npy'
 file_datasetKDEFNoLog = 'dataset/kdef_dataset12NoLog.npy' 
 file_labelsKDEFNoLog =  'dataset/fdef_labels12NoLog.npy'
 
-file_dataset = file_dataset12
-file_labels = file_labels12
-use_log = True
+kdef_nolog_1318 = 'dataset/kdef_nolog_1318.npy'
+kdef_labels_nolog_1318 = 'dataset/kdef_labels_nolog_1318.npy'
+
+if dataset=='CAFE':
+    file_dataset = CAFE_data
+    file_labels = CAFE_labels
+elif dataset=='JAFFE':
+    file_dataset = JAFFE_data
+    file_labels = JAFFE_labels
+elif dataset=='KDEF':
+    file_dataset = KDEF_data
+    file_labels = KDEF_labels
+elif dataset=='UNION':
+    file_dataset = UNION_data
+    file_labels = UNION_labels
+
 
